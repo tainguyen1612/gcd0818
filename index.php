c245ac6cfcb2e1d9db1064c7629af24d66068fdf
hvphi <hvphivh@gmail.com>
2019-06-05 11:48:37 +0700

    Doc csdl

--- index.php
+++ index.php
@@ -7,6 +7,44 @@
     putenv("DATABASE_URL=GR");
     echo getenv("DATABASE_URL")
  ?> 
-     
+    
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "FPT Greenwich";
+$x = 5;
+$y = 5;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+
+
+
+if (empty(getenv("DATABASE_URL"))){
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+}  else {
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+
+$sql = "SELECT id, name FROM label";
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+
+foreach ($resultSet as $row) {
+	echo $row['name'] . '\n';
+}
+
+?>
  </body>
 </html>
