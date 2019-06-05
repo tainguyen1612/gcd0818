6b7f07fcdaa1c61cc370ad8c10ecad20e6304392
hvphi <hvphivh@gmail.com>
2019-06-05 13:31:29 +0700

    Doc csdl

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (empty(getenv("DATABASE_URL"))){
      echo getenv("DATABASE_URL");
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        "host=%ec2-174-129-240-67.compute-1.amazonaws.com;port=%5432;user=%wrflrxtavasvqh;password=%fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b;dbname=%d3mmhribgmc6bf",
         $db["host"],
         $db["port"],
         $db["user"],
