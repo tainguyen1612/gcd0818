3eab6db48b0d2ce4532bb781c0f2cb053af6cbc3
hvphi <hvphivh@gmail.com>
2019-06-05 16:09:19 +0700

    Doc csdl

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (empty(getenv("DATABASE_URL"))){
     $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
 }  else {
      echo '<p>Da co CSDL</p>';
-     echo getenv("DATABASE_URL");
+     //echo getenv("DATABASE_URL");
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
         "host=ec2-174-129-240-67.compute-1.amazonaws.com;port=5432;user=wrflrxtavasvqh;password=fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b;dbname=d3mmhribgmc6bf",
@@ -45,7 +45,9 @@ $stmt->execute();
 $resultSet = $stmt->fetchAll();
 
 foreach ($resultSet as $row) {
-	echo $row['name'] . '\n';
+	echo $row['id'];
+        echo $row['name'];
+        echo '\n';
 }
 
 ?>
