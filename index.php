52db8251e495d410895593f20efddcf9372bbe89
hvphi <hvphivh@gmail.com>
2019-06-06 08:56:55 +0700

    Doc csdl

--- index.php
+++ index.php
@@ -22,11 +22,11 @@ if (empty(getenv("DATABASE_URL"))){
    //        "host=ec2-174-129-240-67.compute-1.amazonaws.com;port=5432;user=wrflrxtavasvqh;password=fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b;dbname=d3mmhribgmc6bf",
    $pdo = new PDO(
 
-        'ec2-174-129-240-67.compute-1.amazonaws.com',//host
-        'd3mmhribgmc6bf',//name
-        '5432',//port
-        'wrflrxtavasvqh',//user
-        'fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b'//password
+        'pgsql:host = ec2-174-129-240-67.compute-1.amazonaws.com',//host
+        'dbname = d3mmhribgmc6bf',//name
+        'port = 5432',//port
+        'user = wrflrxtavasvqh',//user
+        'password = fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b'//password
      
    );
 }  
