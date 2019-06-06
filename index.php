48eca69a61bcc959cf0dde90226d1302f8b26f73
hvphi <hvphivh@gmail.com>
2019-06-06 08:30:07 +0700

    Doc csdl

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
    // echo getenv("DATABASE_URL")
 
  ?> 
-    <a href="https://cloud-netbean.herokuapp.com/ConnectToDB" target="_blank" >Connect database</a> 
+     <a href="ConnectToDB.php" target="_blank" >Connect database</a> 
 <?php
 
 
