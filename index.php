bb0d92e74faafa87e7612db5acbfaf29f696cbf7
hvphi <hvphivh@gmail.com>
2019-06-06 09:30:02 +0700

    Doc csdl

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  
  ?> 
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
-     <a href="InsertData.php.php" target="_blank" >Insert data to the database</a> 
+     <a href="InsertData.php" target="_blank" >Insert data to the database</a> 
 
  </body>
 </html>
