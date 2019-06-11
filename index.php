a1c1b82093a64576fe89ec78a69b2d90c5bf6cc4
taulatai123 <taino846@gmail.com>
2019-06-11 16:53:06 +0700

    fix index.php

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
  <?php echo '<p>Hello. This is first cloud app</p>'; 
  
  ?> 
+     <h1>Hello World </h1>
+     <h2>This is Cloud Computing class</h2>
+     <a href="vnexpress.net" target="_black" >VnExpress Page</a>
+     <a href="24h.com.vn" target="_black" >24h Page</a>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
