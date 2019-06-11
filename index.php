8f5dc28e80c7a0f6bd5be7f7091dfee1d987a41a
taulatai123 <taino846@gmail.com>
2019-06-11 16:55:40 +0700

    fix index.php

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
  ?> 
      <h1>Hello World </h1>
      <h2>This is Cloud Computing class</h2>
-     <a href="vnexpress.net" target="_black" >VnExpress Page</a>
-     <a href="24h.com.vn" target="_black" >24h Page</a>
+     <a href="https://vnexpress.net/" target="_black" >VnExpress Page</a>
+     <br/>
+     <a href="https://www.24h.com.vn/" target="_black" >24h Page</a>
+     <br/>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
